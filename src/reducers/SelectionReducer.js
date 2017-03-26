e5960a03c8fa23486dfe77481ffee39358629fb5
Vishwesh <vishwesh03071998@gmail.com>
2017-03-27 00:52:14 +0530

    Added SelectionReducer.

---
+++ src/reducers/SelectionReducer.js
@@ -0,0 +1,4 @@
+export default () => {
+	//returning null as in inilial state we don't know which movie is selected
+  	return null;
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import MovieReducer from './MovieReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-  movies: MovieReducer
+  movies: MovieReducer,
+  selectedMovieTitle: SelectionReducer,
 });
 
