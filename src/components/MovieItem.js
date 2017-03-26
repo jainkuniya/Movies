f37dc715e57d86bcec8bb7215295f38c6a75960f
Vishwesh <vishwesh03071998@gmail.com>
2017-03-27 00:10:09 +0530

    Added MovieItem.

---
+++ src/components/MovieItem.js
@@ -0,0 +1,12 @@
+//Import libraries
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+//create class component
+export default class MovieItem extends Component {
+  render() {
+  	return (
+  		<Text>Movie</Text>
+  	);
+  }
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import MovieItem from './MovieItem';
 
 class MovieList extends Component {
 	componentWillMount() {
 	    const ds = new ListView.DataSource({
 	      rowHasChanged: (r1, r2) => r1 !== r2
 	    });
-
-	    this.dataSource = ds.cloneWithRows(this.props.movies);
+		this.dataSource = ds.cloneWithRows(this.props.movies);
 	}
 
-	renderRow() {
-		//as nothing is returned it will throw error
+	renderRow(movie) {
+		return (<MovieList
+			movie={movie}
+		/>);
 	}
 
 	render() {
